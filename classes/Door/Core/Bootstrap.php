c5ac24b5af7ed7323b25d889df25b3f5fe7113c7
serginho <serginho@serginhoPC.Dlink>
2014-05-17 17:55:33 +0400

    next

---
+++ classes/Door/Core/Bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Created by Sachik Sergey
+ * box@serginho.ru
+ */
+
+namespace Door\Core;
+ 
+/**
+ * Helper for bootstrap Door framework
+ *
+ * @author serginho
+ */
+class Bootstrap {
+
+	protected static $core_libraries = array(
+		"router" => '/Door/Core/Library/Router',
+		"views" => '/Door/Core/Library/Views',
+		"media" => '/Door/Core/Library/Media',
+		"html" => '/Door/Core/Library/HTML',
+		"url" => '/Door/Core/Library/URL',
+		"auth" => '/Door/Core/Library/Auth',
+		"session" => '/Door/Core/Library/Session',
+		"cookie" => '/Door/Core/Library/Cookie',
+		"database" => '/Door/Core/Library/Database',
+		"models" => '/Door/Core/Library/Models',
+		"lang" => '/Door/Core/Library/Lang'
+	);	
+	
+	public static function register_core_libraries(Application $app)
+	{
+		foreach(self::$core_libraries as $library => $class_name)
+		{
+			if( ! $app->library_exists($library))
+			{
+				$app->register_library($library, $class_name);
+			}
+		}		
+	}
+	
+	
+}
